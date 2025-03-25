b373f473ee2f70bac8b6b80b1547d9d7d297c5b8
Esmaeil Nemati <nemati.esmaeil1370@gmail.com>
2025-03-26 02:18:39 +0330

    asas

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -3,5 +3,9 @@
     public class Class1
     {
         public int MyProperty { get; set; }
+        public int MyProperty1 { get; set; }
+        public int MyProperty2 { get; set; }
+        public int MyProperty3 { get; set; }
+        public int MyProperty4 { get; set; }
     }
 }
