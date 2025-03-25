99bd2d28bdc9f2f7c59cddc70b5ee3497cd5bf4e
Esmaeil Nemati <97082977+EsmaeilNemati@users.noreply.github.com>
2025-03-26 02:19:11 +0330

    Update Class1.cs

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -3,9 +3,6 @@
     public class Class1
     {
         public int MyProperty { get; set; }
-        public int MyProperty1 { get; set; }
-        public int MyProperty2 { get; set; }
-        public int MyProperty3 { get; set; }
-        public int MyProperty4 { get; set; }
+   
     }
 }
